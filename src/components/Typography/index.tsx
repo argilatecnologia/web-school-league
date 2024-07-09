08249795d7923556297cd38f2c35faa366c8c920
Lubnnia <lubnnia@gmail.com>
2024-07-09 11:53:22 -0300

    chore: Create function to return Text and Heading typograph

--- src/components/Typography/index.tsx
+++ src/components/Typography/index.tsx
@@ -19,7 +19,7 @@ interface ITextProps {
 interface IHeadingProps {
   children: ReactNode;
   size: 'xs' | 'sm' | 'md' | 'lg' | '3xl' | '4xl';
-  color?:
+  color:
     | 'gray-100'
     | 'gray-200'
     | 'gray-300'
